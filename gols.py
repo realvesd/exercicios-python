e0b975f3080c0b559a49174afafa0e24bf74a045
realvesd <43302742+realvesd@users.noreply.github.com>
2018-10-09 21:26:40 -0300

    Create gols.py

---
+++ gols.py
@@ -0,0 +1,9 @@
+golsport = int(input("Quantos gols o sport fez? ")) 
+.. golnaut = int(input("Quantos gols o nautico fez? ")) 
+..  
+.. if golsport > golnaut: 
+..   print("O vencedor é o sport!") 
+.. elif golnaut > golsport: 
+..   print("O vencedor é o náutico!") 
+.. else: 
+..   print("O resultado foi um empate!")
