39d372e4c92a0f7fa202685a4b51f2cfe3b075cc
realvesd <43302742+realvesd@users.noreply.github.com>
2018-10-10 20:36:13 -0300

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,10 @@
+a = int(input("Informe um valor inteiro para a: "))
+b = int(input("Informe um valor inteiro para b: "))
+c = int(input("Informe um valor inteiro para c: "))
+
+if ((a + b) < c):
+  print("A soma de a e b é menor que c!")
+elif ((a + b) > c):
+  print("A soma de a e b é maior que c!")
+else:
+  print("A soma de a e b é igual a c")
