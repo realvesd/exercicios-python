589272d152739f7258a045e31b5e74249e48d0d9
realvesd <43302742+realvesd@users.noreply.github.com>
2018-10-09 21:25:31 -0300

    Create nota.py

---
+++ nota.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Informe sua primeira nota: "))
+nota2 = float(input("Informe sua segunda nota: "))
+nota3 = float(input("Informe sua terceira nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+if media >= 5:
+  print("Aprovado")
+else:
+  print("Reprovado")
