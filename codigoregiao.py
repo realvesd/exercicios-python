9f64280c7134ac161c62a344223bb22d65064acc
realvesd <43302742+realvesd@users.noreply.github.com>
2018-10-09 21:34:12 -0300

    Create codigoregiao.py

---
+++ codigoregiao.py
@@ -0,0 +1,12 @@
+cod = str(input("Informe o código do produto: "))
+
+if cod == '1' or '2':
+  print("O produto veio do Sul")
+elif cod == '3' or '4':
+  print("O produto veio do Sudeste")
+elif cod == '5' or '6':
+  print("O produto veio do Norte")
+elif cod == '7' or '8':
+  print("O produto veio do Nordeste")
+elif cod == '9' or '10':
+  print("O produto veio do Centro-Oeste")
