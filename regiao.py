bcc4b62580ca3ef560ca7e713ab0a5ee92b14533
realvesd <43302742+realvesd@users.noreply.github.com>
2018-10-17 14:49:39 -0300

    Create regiao.py

---
+++ regiao.py
@@ -0,0 +1,6 @@
+estado = str(input("Qual o estado? "))
+
+if estado == 'PE' or estado == 'PB':
+  print("O estado pertence a região Nordeste!")
+elif estado == 'SP' or estado == 'RJ':
+  print("O estado pertence a região Sudeste")
