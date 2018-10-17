89a38cf08fcf8d74363ecdbe0a2076f4a095ce5a
realvesd <43302742+realvesd@users.noreply.github.com>
2018-10-17 14:54:21 -0300

    Create codigoproduto.py

---
+++ codigoproduto.py
@@ -0,0 +1,12 @@
+prod = str(input("Qual o código do produto? "))
+
+if prod == '1':
+  print("Pizza!")
+elif prod == '2':
+  print("Hamburger!")
+elif prod == '3':
+  print("Refrigerante!")
+elif prod == '4':
+  print("Batata frita!")
+else:
+  print("Produto não encontrado") 
